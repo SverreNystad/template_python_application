0dd79a88ffb5c2e6319d46ee82a6443adc0c0824
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2023-09-18 08:27:32 +0200

    Feat: Add logger

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 """ The main entrypoint for the application. """
+import logging
 
+# Set up logging
+logging.basicConfig(filename='gpt_dungeon_master.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 def hello_world() -> str:
     """ Returns a friendly greeting. """
     return "Hello World!"

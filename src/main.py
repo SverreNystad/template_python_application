c48dcd4ffae7fc19830c0f37681314fd13b6f7a8
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2023-09-18 08:28:25 +0200

    Refactor: Change name of logging file

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@
 import logging
 
 # Set up logging
-logging.basicConfig(filename='gpt_dungeon_master.log',
+logging.basicConfig(filename='app.log',
                     level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 def hello_world() -> str:
     """ Returns a friendly greeting. """
     return "Hello World!"
